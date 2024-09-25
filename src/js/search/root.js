8d575a56ff0c7464bfba411ba68ecc5d4dc457d9
Haranycheva Mariia <mimimashkabumazka@gmail.com>
2024-09-25 19:00:08 +0300

    side panel

---
+++ src/js/search/root.js
@@ -0,0 +1,5 @@
+export const root = {
+    openModalBtn: document.querySelector("#open-modal-js"),
+    closeModalBtn: document.querySelector("#close-modal-js"),
+    modal: document.querySelector("#modal-js"),
+};
