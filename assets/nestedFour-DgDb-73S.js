723450d3782b588e5fa67becafa5df2d690392e8
haranycheva <120900899+haranycheva@users.noreply.github.com>
2024-12-23 12:23:11 +0000

    Deploying to gh-pages from @ RockMasha/Leaf_Of_Hope@f2c6904b68c73f325cd33ae86ae14d0a5055c10d 🚀

---
+++ assets/nestedFour-DgDb-73S.js
@@ -0,0 +1,132 @@
+import{a as c,h as j}from"./setCurrentLanguage-0Hl_EXOw.js";import{a as B}from"./api-DfaLOtv4.js";import{g as U}from"./getValueSrcParams-BMgebK2A.js";import{g as H,c as d}from"./getDataValue-BE9Ttjkr.js";import{s as R}from"./setSwitchVariableLanguage-XrKfQosM.js";function F(){document.querySelector(".advert-properties_more").classList.toggle("advert-properties_hidden")}function G(e){const t=e["small-250px"],s=e["medium-300px"],r=e["large-500px"];return`
+            <picture class="advert-maininfo__img">
+              <source
+                srcset="
+                ${r} 1x,
+                ${r} 2x"
+                media="(min-width: 1440px)"
+              />
+              <source
+                srcset="
+                ${s} 1x,
+                ${s} 2x
+                "
+                media="(min-width: 768px)"
+              />
+              <source
+                srcset="
+                ${t} 1x,
+                ${t} 2x"
+              />
+              <img
+                src="${t}"
+                srcset="
+                ${t} 1x,
+                ${t} 2x,"
+                class="advert-maininfo__img"
+                alt="plant"
+                loading="lazy"
+              />
+            </picture>
+          `}function J(e){const t=e["tiny-100px"],s=e["very-small-175px"],r=e["small-250px"];return`
+      <picture class="advert-userinfo__img big-photo">
+        <source
+          srcset="
+            ${r} 1x,
+            ${r} 2x"
+          media="(min-width: 1440px)"
+        />
+        <source
+          srcset="
+            ${s} 1x,
+            ${s} 2x"
+          media="(min-width: 768px)"
+        />
+        <source
+          srcset="
+            ${t} 1x,
+            ${t} 2x"
+        />
+        <img
+          src="${t}"
+          srcset="
+            ${t} 1x,
+            ${t} 2x,"
+          class="advert-userinfo__img big-photo"
+          alt="plant"
+          loading="lazy"
+        />
+      </picture>`}let a;function K(e){const t=H(e),{image:s,name:r,description:i,wish:n,date:v,lang:m,toxicity:_,plantCondition:g,plantType:h,height:f,light:$,temperature:x,watering:w,care:y,substrate:b,windowDistance:L,allergenicity:S,growthRate:E,lifeDuration:I,rarity:P,username:T,email:q,avatar:D,adress:A,phone:W,way:M}=t,O=c();a=c()==="or"?m:O;const{pageName:V,descriptionTitle:k,defaultDescription:z,WishTitle:C,defaultWish:N}=Q();return`
+          <div class="container advert__container">
+            <h1 hidden>${V}</h1>
+            <div class="advert-maininfo">
+              ${G(s)}
+              <div>
+                <p class="advert-maininfo__date">${v}</p>
+                <h2 class="advert-maininfo__title">${r}</h2>
+                <div class="advert-maininfo-way">
+                  <p class="advert-maininfo-way__text">${d.way[M][a]}</p>
+                </div>
+                <ul class="advert-properties">
+                  ${o([{toxicity:_},{plantCondition:g},{plantType:h},{height:f}])}
+                  ${o([{light:$},{temperature:x},{watering:w}],!1)}
+                </ul>
+              </div>
+            </div>
+            <ul class="advert-properties advert-properties_more advert-properties_hidden">
+              <li>
+                <ul class="advert-properties__more-list">
+                  ${o([{care:y},{substrate:b},{windowDistance:L},{growthRate:E}])}
+
+                </ul>
+              </li>
+              <li>
+                <ul class="advert-properties__more-list">
+                  ${o([{allergenicity:S},{lifeDuration:I},{rarity:P}])}
+                </ul>
+              </li>
+            </ul>
+            <button class="advert-properties__more-btn" aria-label="more properties">
+              <ul class="advert-properties-cross">
+                <li class="advert-properties-cross__line advert-properties-cross__line_vertical"></li>
+                <li class="advert-properties-cross__line advert-properties-cross__line_horizontal"></li>
+              </ul>
+            </button>
+            <div class="advert-discreption">
+              <h2 class="advert-discreption__title">${k}</h2>
+              <p class="advert-discreption__text">
+                ${i||`<span class="advert-discreption__text_default">${z}</span>`}
+              </p>
+            </div>
+            <div class="advert-user">
+              <div class="advert-userinfo">
+                <div class="advert-userinfo__profile-img">
+                  ${J(D)}
+                </div>
+                <div class="advert-userinfo__userinfos">
+                  <h2 class="advert-userinfo__name">${T}</h2>
+                  <p class="advert-userinfo__adress">${A}</p>
+                  <p class="advert-userinfo__phone">
+                    ${W}
+                  </p>
+                  <p class="advert-userinfo__email">
+                    ${q}
+                  </p>
+                </div>
+              </div>
+              <div class="advert-wishes ${e.way==="exchange"?"":"is-hidden"}">
+                <h2 class="advert-wishes__title">${C}</h2>
+                <p class="advert-wishes__text">
+                 ${n||`<span class="advert-wishes__text_default">${N}</span>`}
+                </p>
+              </div>
+            </div>
+          </div>`}function Q(){const e={};return e.pageName=a==="ua"?"Оголошення":"Advert",e.descriptionTitle=a==="ua"?"Опис":"Description",e.defaultDescription=a==="ua"?"Немає опису":"There is no description",e.WishTitle=a==="ua"?"Побажання":"Wish",e.defaultWish=a==="ua"?"Немає побажань":"There are no wishes",e}function o(e,t=!0){const s=[];for(const r of e){const i=Object.keys(r)[0];let n=t?X(r,i):Y(r,i);s.push(n)}return s.join("")}function X(e,t){return`
+  <li class="advert-properties__item">
+    <p class="advert-properties__text">${p(t)}: ${e[t]}</p>
+  </li>`}function Y(e,t){return`
+  <li class="advert-properties__item">
+    <p class="advert-properties__text"> 
+    ${p(t)}: <span>${e[t]}</span>
+    </p>
+  </li>`}function p(e){return d[e].name[a]}const Z={translateWrapp:document.querySelector(".translate-wrapper_variable"),translate:document.querySelector(".translate"),translateInput:document.querySelector(".translate__input"),main:document.querySelector("main"),advert:document.querySelector(".advert")};let u={};async function ee(){const e=U("id");u=await B(e),l()}function l(){const e=K(u);Z.advert.innerHTML=e,te()}function te(){document.querySelector(".advert-properties__more-btn").addEventListener("click",F)}const re=l,se=l,ae=l,ie={setUkrainian:re,setEnglish:se,setOriginal:ae};await ee();R(ie);j();
