855ab6706fa344e6604cf948f2936ab9eb37bb3f
Maria <maryallbond@gmail.com>
2025-01-11 21:35:51 +0200

    debag errors

--- src/js/universal/translate/universal/templeSettings/propertiesSettings.js
+++ src/js/universal/translate/universal/templeSettings/propertiesSettings.js
@@ -108,6 +108,10 @@ export const propertiesSettings = {
     ua: `теплолюбні`,
     en: `heat-loving`,
   },
+  temperatureMediumText: {
+    ua: `cередньотривалі`,
+    en: `medium-duration`,
+  },
   temperatureColdResistantText: {
     ua: `холодостійкі`,
     en: `cold-resistant`,
