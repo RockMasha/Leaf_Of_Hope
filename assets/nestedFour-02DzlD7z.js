b50210e9f776fd88e8304297c1a7cd3983e4b195
haranycheva <120900899+haranycheva@users.noreply.github.com>
2025-02-02 22:08:46 +0000

    Deploying to gh-pages from @ RockMasha/Leaf_Of_Hope@b1c131d02bf96715aaefd1947765f73a4ab9cd2d 🚀

--- assets/nestedFour-02DzlD7z.js
+++ assets/nestedFour-02DzlD7z.js
@@ -1,4 +1,4 @@
-import{a as c,h as j}from"./setCurrentLanguage-DdORZCPD.js";import{a as B}from"./api-B7v0WwrB.js";import{g as U}from"./getValueSrcParams-BMgebK2A.js";import{c as d}from"./cardData-CVkVixM2.js";import{g as H}from"./getDataValue-07PZ5mIk.js";import{s as R}from"./setSwitchVariableLanguage-C4UeiRFq.js";function F(){document.querySelector(".advert-properties_more").classList.toggle("advert-properties_hidden")}function G(e){const t=e["small-250px"],s=e["medium-300px"],r=e["large-500px"];return`
+import{a as c,h as j}from"./setCurrentLanguage-DdORZCPD.js";import{a as B}from"./api-B7v0WwrB.js";import{g as U}from"./getValueSrcParams-BMgebK2A.js";import{c as d}from"./cardData-CVkVixM2.js";import{g as H}from"./getDataValue-COy2JZd_.js";import{s as R}from"./setSwitchVariableLanguage-C4UeiRFq.js";function F(){document.querySelector(".advert-properties_more").classList.toggle("advert-properties_hidden")}function G(e){const t=e["small-250px"],s=e["medium-300px"],r=e["large-500px"];return`
             <picture class="advert-maininfo__img">
               <source
                 srcset="
