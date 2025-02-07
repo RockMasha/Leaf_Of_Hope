1ea259f522de90bae7131e70217e0587f70fe43c
Maria <maryallbond@gmail.com>
2025-02-07 22:51:34 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -20,6 +20,10 @@ export default defineConfig({
         nestedEight: resolve(__dirname, "src/criteria.html"),
         nestedNine: resolve(__dirname, "src/authentication.html"),
         nestedTen: resolve(__dirname, "src/metabase.html"),
+        nestedEleven: resolve(
+          __dirname,
+          "https:/my-mbasedomain.site/embed/dashboard/"
+        ),
       },
     },
     outDir: "../dist",
