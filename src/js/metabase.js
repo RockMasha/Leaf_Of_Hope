52e90712be47ef79566169bcc90ac0a41297e84f
Maria <maryallbond@gmail.com>
2025-02-20 20:06:36 +0200

    change arrow signin svg

--- src/js/metabase.js
+++ src/js/metabase.js
@@ -2,8 +2,6 @@ import { getMetabaseIframe } from "./servise/api";
 import { hiddenContentLoader } from "./universal/hiddenContentLoader";
 
 const { iframeUrl } = await getMetabaseIframe();
-console.log(iframeUrl);
-
 const mainEl = document.querySelector("main");
 mainEl.innerHTML = `<iframe src="${iframeUrl}" frameborder="0" width="100%" height="100%" allowtransparency ></iframe>`;
 
