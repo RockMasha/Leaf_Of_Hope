c625f74844d93f999724e5cf6a52a19ea2d7a16d
Maria <maryallbond@gmail.com>
2025-04-25 20:34:38 +0300

    update metabase

--- src/js/metabase.js
+++ src/js/metabase.js
@@ -1,8 +1,13 @@
 import { getMetabaseIframe } from "./servise/api";
 import { hiddenContentLoader } from "./universal/hiddenContentLoader";
 
-const { iframeUrl } = await getMetabaseIframe();
-const mainEl = document.querySelector("main");
-mainEl.innerHTML = `<iframe src="${iframeUrl}" frameborder="0" width="100%" height="100%" allowtransparency ></iframe>`;
+try {
+  const { iframeUrl } = await getMetabaseIframe();
+  const mainEl = document.querySelector("main");
+  mainEl.innerHTML = `<iframe src="${iframeUrl}" frameborder="0" width="100%" height="100%" allowtransparency ></iframe>`;
+  mainEl.setAttribute("metabase", "");
+} catch (error) {
+  console.log("error", error);
+}
 
 hiddenContentLoader();
