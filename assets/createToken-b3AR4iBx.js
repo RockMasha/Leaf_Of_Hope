fd17c50564d3264523cfd8a3965f35b8970718fc
haranycheva <120900899+haranycheva@users.noreply.github.com>
2025-06-06 08:48:19 +0000

    Deploying to gh-pages from @ RockMasha/Leaf_Of_Hope@4c62d6e45c81cf88c13e417c14ab35d90e0dcc4d 🚀

---
+++ assets/createToken-b3AR4iBx.js
@@ -0,0 +1,1 @@
+import{t}from"./api-C4gWJdAs.js";function a(e){localStorage.setItem("token",`${e}`),t()}export{a as c};
---
+++ assets/nestedTen-BOnYRRuh.js
@@ -0,0 +1,1 @@
+import{h as t}from"./getCurrentLanguage-BQe-LnNi.js";import{u as a}from"./api-C4gWJdAs.js";try{const{iframeUrl:r}=await a(),e=document.querySelector("main");e.innerHTML=`<iframe src="${r}" frameborder="0" width="100%" height="100%" allowtransparency ></iframe>`,e.setAttribute("metabase","")}catch(r){console.log("error",r)}t();
